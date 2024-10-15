7fa6ae460ba38c6c242f50904a2e0f00e202de08
Rin <pelikhovmn@yandex.ru>
2024-10-15 17:18:38 +0300

    Создана функция для получения изображения с котиком

--- catas.py
+++ catas.py
@@ -1,8 +1,24 @@
 from cProfile import label
+from cgitb import reset
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+from Scripts.bottle import response
+
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_date = BytesIO(response.content)
+        img = Image.open(image_date)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+
 
 window = Tk()
 window.title("Cats!")
