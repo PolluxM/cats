cbbe78e1aad88298a68393c3dd9e4e13cabda89e
Rin <pelikhovmn@yandex.ru>
2024-10-15 18:02:13 +0300

    Добавили окно ввода тега, изменили функию принимать тег и выдавать картинку по тегу, так же добавили если нечего не ввести в поле то выводить просто в случайном выборе

--- catas.py
+++ catas.py
@@ -21,8 +21,9 @@ def load_image(url):
         return None
 
 def open_new_window():
-    img = load_image(url)
-
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котиком")
@@ -37,6 +38,13 @@ def exit():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
+
+# Поле ввода для тегов
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 # update_button = Button(text="Обновить", command=set_image)  Кнопка
 # update_button.pack()
@@ -53,7 +61,5 @@ file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
 img = load_image(url)
-
-set_image()
 
 window.mainloop()
